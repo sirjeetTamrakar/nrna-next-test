d2d119436c693c29ab679d7d885304bd345bd829
Srijeet Tamrakar <sirjeettamrakar007@gmail.com>
2024-02-20 17:21:11 +0545

    some change

--- src/pages/news/[slug].js
+++ src/pages/news/[slug].js
@@ -1,37 +1,27 @@
 import SecondaryNav from "@/components/locals/News/SecondaryNav";
 import { getAllNews, getNewsCategory } from "@/redux/homepage/actions";
 import { changeDateFormat } from "@/utils/dateUtils";
-import { WhatsApp } from "@mui/icons-material";
-import FacebookIcon from "@mui/icons-material/Facebook";
-import TwitterIcon from "@mui/icons-material/Twitter";
-import { Box, CircularProgress, Grid } from "@mui/material";
+import { CircularProgress, Grid } from "@mui/material";
 import Head from "next/head";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import {
-  FacebookShareButton,
-  TwitterShareButton,
-  WhatsappShareButton,
-} from "react-share";
 
-const SingleNews = ({ single_news = {} }) => {
-  console.log({ single_news });
+const SingleNews = ({ single_news }) => {
   const dispatch = useDispatch();
   const location = useRouter();
-  const { slug } = location?.query;
-  const pathname = window.location.pathname;
-
-  console.log({ location });
+  const { slug } = location.query;
 
   useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
+    dispatch(getAllNews());
+    dispatch(getNewsCategory());
+  }, [slug, dispatch]);
 
   const { news, news_category, single_news_loading } = useSelector(
     (state) => state.homepage
   );
+
   const recentNews = news?.data
     ?.filter((list) => list?.slug !== slug)
     .slice(0, 4);
@@ -42,46 +32,18 @@ const SingleNews = ({ single_news = {} }) => {
       : news_category?.[0]?.id
   );
 
-  // useEffect(() => {
-  //   dispatch(getSingleNews(slug));
-  // }, [slug]);
-  useEffect(() => {
-    dispatch(getAllNews());
-    dispatch(getNewsCategory());
-  }, [slug]);
-
-  const handleSocialClick = () => {};
-
   return (
     <>
       <Head>
-        <meta
-          name="og:description"
-          content={single_news?.excerpt}
-          key={"ogdescription"}
-        />
-        <meta
-          property="og:image"
-          content={single_news?.feature_image}
-          key={"ogimage"}
-        />
-        <meta
-          property="og:title"
-          content={"रविना थापाद्वारा एनआरएनए अध्यक्षमा उम्मेदवारी दर्ता"}
-          key={"ogtitle"}
-        />
-
+        <meta property="og:description" content={single_news?.excerpt} />
+        <meta property="og:image" content={single_news?.feature_image} />
+        <meta property="og:title" content={single_news?.title} />
         <meta
           property="og:url"
           content={`https://nrna-next-test.vercel.app/news/${single_news?.slug}`}
-          key={"ogurl"}
         />
-        <meta property="og:type" content="article" key={"ogtype"} />
-        <title>{"रविना थापाद्वारा एनआरएनए अध्यक्षमा उम्मेदवारी दर्ता"}</title>
-        <meta name="description" content={single_news?.excerpt} />
-
-        <meta property="author" content={single_news?.author} />
-        <meta name="twitter:card" content={"summary_large_image"} />
+        <meta property="og:type" content="article" />
+        <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content={single_news?.title} />
         <meta name="twitter:description" content={single_news?.excerpt} />
         <meta name="twitter:image" content={single_news?.feature_image} />
@@ -99,83 +61,34 @@ const SingleNews = ({ single_news = {} }) => {
 
       <div className="container">
         {single_news_loading ? (
-          <Box
-            display="flex"
-            justifyContent="center"
-            alignItems="center"
-            sx={{ height: "60vh" }}
-          >
-            <CircularProgress size={30} />
-          </Box>
+          <CircularProgress />
         ) : (
           <div className="single_news_page">
             <Grid container className="news_main_grid">
               <Grid item md={8}>
                 <div className="single_news_page_content">
-                  <>
-                    <div className="single_news_page_imgwrap">
-                      <img
-                        src={single_news?.feature_image}
-                        alt={single_news?.title}
-                      />
-                    </div>
-                    <div className="single_news_page_title">
-                      {single_news?.title}
-                    </div>
-
-                    <div className="single_news_page_date">
-                      {changeDateFormat(
-                        single_news?.created_at,
-                        "DD-MMM-YYYY HH:MM"
-                      )}{" "}
-                      | Author: {single_news?.author ?? "NBNS Global"}
-                    </div>
-                    <Box
-                      sx={{
-                        display: "flex",
-                        flexDirection: "column",
-                        marginTop: "20px",
-                      }}
-                    >
-                      <Box sx={{ color: "gray", fontSize: "12px" }}>
-                        Share News
-                      </Box>
-                      <Box
-                        sx={{ display: "flex", gap: "15px", marginTop: "10px" }}
-                      >
-                        <FacebookShareButton
-                          appId="393640856408187"
-                          onClick={handleSocialClick}
-                          url={`https://nrna-next-test.vercel.app/news/${single_news?.slug}`}
-                        >
-                          <FacebookIcon
-                            sx={{ color: "#0866FF", fontSize: "30px" }}
-                          />
-                        </FacebookShareButton>
-                        <TwitterShareButton
-                          url={`https://nrna-next-test.vercel.app/news/${single_news?.slug}`}
-                        >
-                          <TwitterIcon
-                            sx={{ color: "#1BC4F7", fontSize: "30px" }}
-                          />
-                        </TwitterShareButton>
-                        <WhatsappShareButton
-                          url={`https://nrna-next-test.vercel.app/news/${single_news?.slug}`}
-                        >
-                          <WhatsApp
-                            sx={{ color: "#24CC63", fontSize: "30px" }}
-                          />
-                        </WhatsappShareButton>
-                      </Box>
-                    </Box>
-
-                    <div
-                      className="single_news_page_long"
-                      dangerouslySetInnerHTML={{
-                        __html: single_news?.description,
-                      }}
-                    ></div>
-                  </>
+                  <div className="single_news_page_imgwrap">
+                    <img
+                      src={single_news?.feature_image}
+                      alt={single_news?.title}
+                    />
+                  </div>
+                  <div className="single_news_page_title">
+                    {single_news?.title}
+                  </div>
+                  <div className="single_news_page_date">
+                    {changeDateFormat(
+                      single_news?.created_at,
+                      "DD-MMM-YYYY HH:MM"
+                    )}{" "}
+                    | Author: {single_news?.author ?? "NBNS Global"}
+                  </div>
+                  <div
+                    className="single_news_page_long"
+                    dangerouslySetInnerHTML={{
+                      __html: single_news?.description,
+                    }}
+                  />
                 </div>
               </Grid>
               <Grid item md={4} sm={12} className="recent_news">
@@ -234,16 +147,10 @@ export async function getStaticProps({ params }) {
     props: {
       single_news: res?.data,
     },
-    // Next.js will attempt to re-generate the page:
-    // - When a request comes in
-    // - At most once every 10 seconds
     revalidate: 10, // In seconds
   };
 }
 
-// This function gets called at build time on server-side.
-// It may be called again, on a serverless function, if
-// the path has not been generated.
 export async function getStaticPaths() {
   const res = await fetch("https://api.nrnaglobal.com/api/news")
     .then((response) => response.json())
