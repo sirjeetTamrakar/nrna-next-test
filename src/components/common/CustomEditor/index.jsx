5bf9fd3b1ea0208b654a9bfc3677ab0fd46da980
Srijeet Tamrakar <sirjeettamrakar007@gmail.com>
2024-02-22 15:18:19 +0545

    ddd

--- src/components/common/CustomEditor/index.jsx
+++ src/components/common/CustomEditor/index.jsx
@@ -14,27 +14,15 @@ const CustomEditor = ({ name, emailTemplate }) => {
   const { CKEditor, ClassicEditor } = editorRef.current || {};
 
   useEffect(() => {
-    // Dynamically import CKEditor and ClassicEditor
-    const loadEditors = async () => {
-      const { CKEditor: importedCKEditor } = await import(
-        "@ckeditor/ckeditor5-react"
-      );
-      const { default: importedClassicEditor } = await import(
-        "@ckeditor/ckeditor5-build-classic"
-      );
-      editorRef.current = {
-        CKEditor: importedCKEditor,
-        ClassicEditor: importedClassicEditor,
-      };
-      setEditorLoaded(true);
+    editorRef.current = {
+      CKEditor: require("@ckeditor/ckeditor5-react").CKEditor, //Added .CKEditor
+      ClassicEditor: require("@ckeditor/ckeditor5-build-classic"),
     };
-
-    loadEditors();
+    setEditorLoaded(true);
   }, []);
-
   return (
     <>
-      {editorLoaded && CKEditor && ClassicEditor && (
+      {editorLoaded ? (
         <Box>
           <Controller
             name={name}
@@ -69,9 +57,89 @@ const CustomEditor = ({ name, emailTemplate }) => {
             </FormHelperText>
           )}
         </Box>
+      ) : (
+        <></>
       )}
     </>
   );
 };
 
 export default CustomEditor;
+
+// import { Box, FormHelperText, Typography } from "@mui/material";
+// import { useEffect, useRef, useState } from "react";
+// import { Controller, useFormContext } from "react-hook-form";
+// import style from "./style";
+
+// const CustomEditor = ({ name, emailTemplate }) => {
+//   const classes = style();
+//   const {
+//     control,
+//     formState: { errors },
+//   } = useFormContext();
+//   const editorRef = useRef();
+//   const [editorLoaded, setEditorLoaded] = useState(false);
+//   const { CKEditor, ClassicEditor } = editorRef.current || {};
+
+//   useEffect(() => {
+//     // Dynamically import CKEditor and ClassicEditor
+//     const loadEditors = async () => {
+//       const { CKEditor: importedCKEditor } = await import(
+//         "@ckeditor/ckeditor5-react"
+//       );
+//       const { default: importedClassicEditor } = await import(
+//         "@ckeditor/ckeditor5-build-classic"
+//       );
+//       editorRef.current = {
+//         CKEditor: importedCKEditor,
+//         ClassicEditor: importedClassicEditor,
+//       };
+//       setEditorLoaded(true);
+//     };
+
+//     loadEditors();
+//   }, []);
+
+//   return (
+//     <>
+//       {editorLoaded && CKEditor && ClassicEditor && (
+//         <Box>
+//           <Controller
+//             name={name}
+//             control={control}
+//             render={({ field: { onChange, value } }) => (
+//               <div className={classes.richTextEditor}>
+//                 <Typography className={classes.descriptionLable}>
+//                   {emailTemplate ? emailTemplate : "Description"}{" "}
+//                 </Typography>
+//                 <CKEditor
+//                   onReady={(editor) => {
+//                     //  editor.ui.view.editable.element.style.height = "200px";
+//                   }}
+//                   editor={ClassicEditor}
+//                   data={value}
+//                   onChange={(event, editor) => {
+//                     const data = editor.getData();
+//                     onChange(data);
+//                   }}
+//                   onBlur={(event, editor) => {}}
+//                   onFocus={(event, editor) => {}}
+//                 />
+//               </div>
+//             )}
+//           />
+//           {errors?.[name]?.message && (
+//             <FormHelperText
+//               // error={true}
+//               sx={{ fontSize: "10px", color: "red", marginTop: "5px" }}
+//             >
+//               {errors?.[name].message}
+//             </FormHelperText>
+//           )}
+//         </Box>
+//       )}
+//     </>
+//   );
+// };
+
+// export default CustomEditor;
