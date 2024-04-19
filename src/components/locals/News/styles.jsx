4e9b54be02ae01c1bf9c24214f2b1c91b71e0926
Srijeet Tamrakar <sirjeettamrakar007@gmail.com>
2024-04-19 13:05:27 +0545

    Text overflow

--- src/components/locals/News/styles.jsx
+++ src/components/locals/News/styles.jsx
@@ -1,48 +1,49 @@
-import { makeStyles } from '@mui/styles';
+import { makeStyles } from "@mui/styles";
 
 export const useStyles = makeStyles((theme) => ({
   root: {
-    background: '#276fc4'
+    background: "#276fc4",
   },
   header: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    '& input': {
-      height: '16px',
-      background: '#5d9fee',
-      color: '#fff',
-      borderRadius: '4px'
-    }
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    "& input": {
+      height: "16px",
+      background: "#5d9fee",
+      color: "#fff",
+      borderRadius: "4px",
+    },
   },
   contactRoot: {},
   title: {
-    fontSize: '24px !important',
-    color: '#fff',
-    fontWeight: '600 !important',
-    lineHeight: '80% !important'
+    fontSize: "24px !important",
+    color: "#fff",
+    fontWeight: "600 !important",
+    lineHeight: "80% !important",
   },
   list: {
-    listStyle: 'none',
-    display: 'flex',
-    justifyContent: 'flex-start',
-    columnGap: '45px',
+    listStyle: "none",
+    display: "flex",
+    justifyContent: "flex-start",
+    columnGap: "45px",
     margin: 0,
     padding: 0,
-    '& li': {
-      fontSize: '14px',
-      color: '#fff',
-      fontWeight: '400',
-      paddingBottom: '5px',
-      cursor: 'pointer',
-      '&.active': {
-        borderBottom: '4px solid #fff'
-      }
+    "& li": {
+      fontSize: "14px",
+      color: "#fff",
+      fontWeight: "400",
+      paddingBottom: "5px",
+      cursor: "pointer",
+      minWidth: "max-content",
+      "&.active": {
+        borderBottom: "4px solid #fff",
+      },
     },
-    [theme.breakpoints.down('md')]: {
-      overflow: 'auto',
-      overflowY: 'hidden',
-      columnGap: '20px'
-    }
-  }
+    [theme.breakpoints.down("md")]: {
+      overflow: "auto",
+      overflowY: "hidden",
+      columnGap: "20px",
+    },
+  },
 }));
